412c644245e75aebd4aac99bf2c1d5896159b203
hoseasandstrom <Lakyn4ever>
2016-06-16 15:31:44 -0400

    Added tables

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -1,8 +1,63 @@
 package com.studenttheironyard;
 
+import org.h2.tools.Server;
+import spark.Spark;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users( userid IDENTITY, name VARCHAR, email VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS memes(memeid IDENTITY, memename VARCHAR, upvote INT, downvote INT,user_id INT)");
+    }
+
+    public static void insertUser(Connection conn, String name, String password) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES(NULL, ?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, email);
+        stmt.execute();
+    }
+
+    public static ArrayList<User> selectUsers(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM registrations");
+        ResultSet results = stmt.executeQuery();
+        ArrayList<User> userArrayList = new ArrayList<>();
+        while (results.next()) {
+            int userId = results.getInt("userId");
+            String name = results.getString("name");
+            String email = results.getString("email");
+            User user = new User(userId, name, email);
+            userArrayList.add(user);
+        }
+        return userArrayList;
+    }
+
+    public static User selectUser(Connection conn, String name) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE name = ?");
+        stmt.setString(1, name);
+        ResultSet results = stmt.executeQuery();
+        if (results.next()) {
+            int userId = results.getInt("userid");
+            String password = results.getString("password");
+            return new User(userId, name, password);
+        }
+        return null;
+    }
+
+
+    public static void selectMeme(Connection conn, )
+
+
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
+
+        Spark.init();
+
+
     }
 }
