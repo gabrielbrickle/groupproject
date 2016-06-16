a5607ee520eb0f037a5cf1600d2ce8722b8a7f70
hoseasandstrom <Lakyn4ever>
2016-06-16 15:42:47 -0400

    Added tables

---
+++ test/com/studenttheironyard/MainTest.java
@@ -0,0 +1,29 @@
+package com.studenttheironyard;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hoseasandstrom on 6/16/16.
+ */
+public class MainTest {
+    public Connection startConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
+        Main.createTables(conn);
+        return conn;
+    }
+    @Test
+    public void testUser() throws SQLException {
+        Connection conn = startConnection();
+        Main.insertUser(conn, "Alice", "password");
+        User user = Main.selectUser(conn, "Alice");
+        conn.close();
+        assertTrue(user != null);
+    }
+
+}
