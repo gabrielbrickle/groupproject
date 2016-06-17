eeb765bc6a280f88eb5df8d6d9df0dab5c0734db
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-06-16 21:12:26 -0400

    css html and basic js

--- main.js
+++ main.js
@@ -1,87 +1,135 @@
 $(document).ready(function() {
-    doYouLikeMeme.init();
+  doYouLikeMeme.init();
 })
 
 var doYouLikeMeme = {
+  url: 'http://tiny-tiny.herokuapp.com/collections/doyoulikememe',
+  comments: [],
   init: function() {
     doYouLikeMeme.styling();
     doYouLikeMeme.events();
-  },//end init funciton
+  },
   styling: function() {
-    doYouLikeMeme.get();
-  },
+    doYouLikeMeme.getPost();
+  },//end styling
   events: function() {
+//New Comment
+  $('form').submit(function () {
+    event.preventDefault();
+    if ($('input').val() !== '') {
+      var input_value = $(this).find('.messageval').val();
+      var user_value = $(this).find('.usernameval').val();
+      var thingToCreate = {
+        comment: input_value,
+        username: user_value
+      }
+      doYouLikeMeme.createPost(thingToCreate)
+      $('.comments').append(`<li>${user_value}   ${input_value}<a href=""> x</a></li>`);
+    };
+    $('input').val('');
+    return false;
+  })
+  //delete
+  $(document).on('click', 'a', function (element) {
+    event.preventDefault();
+    var commentId = $(this).parent().data('id');
+    console.log("ID", commentId)
+    window.glob = $(this);
+    $(this).parent().remove();
+    doYouLikeMeme.deletePost(commentId);
+  });
 
-  },
+  ////editing a post
+  var newLiVal;
+  $("ul").on('dblclick', 'li', function () {
+    newLiVal = $(this).text();
+    $(this).text("");
+    $("<input type='text' data-id='"+ $(this).data('id') +"'>").appendTo(this).focus();
+  });
+  $("ul").on('focusout', 'li > input', function () {
+    var $this = $(this);
+    var newLiVal = $this.val()
+    $this.text($this.val());
+    $(this).parent().text($this.val()).append('<a href=""> ✓</a>');
+    $this.remove();
+    doYouLikeMeme.editPost({
+      comment: newLiVal,
+      _id: $this.data('id')
+    })
+  });
 
 
-        /////AJAX/////
 
-  create: function(thingToDo) {
-  $.ajax({
-      url: gabeToDo.url,
+
+    },
+
+///begin ajax
+
+  createPost: function(thingCommented) {
+    $.ajax({
+      url: doYouLikeMeme.url,
       method: "POST",
-      data: thingToDo,
+      data: thingCommented,
       success: function(data) {
-          console.log("works", data);
-          gabeToDo.todos.push(data);
-          gabeToDo.getToDo();
+        console.log("works", data);
+          doYouLikeMeme.comments.push(data);
+          doYouLikeMeme.getPost();
       },
       error: function(err) {
-          console.error("OH CRAP", err);
+        console.error("OH CRAP", err);
       }
     })
-          },
+  },
 
-        get: function() {
-            $.ajax({
-                // url:
-                method: "GET",
-                success: function(data) {
-                    console.log("WE GOT SOMETHING", data);
-                    $(".tasksremaining").find('h5').text("Tasks Remaining: " + data.length); ///this counts the list items
-                    $('ul').html("");
-                    data.forEach(function(element, idx) {
-                        var toDoStr = `<li data-id="${element._id}">${element.todo}<a href=""> ✓</a></li>`
-                        $('ul').append(toDoStr)
-                        gabeToDo.todos.push(element);
-                    });
-                },
-                error: function(err) {
-                    console.error("ugh", err);
-                }
-            })
+  getPost: function() {
+    $.ajax({
+      url: doYouLikeMeme.url,
+      method: "GET",
+      success: function(data) {
+        console.log("WE GOT SOMETHING", data);
+        $(".totalcomments").find('h5').text("Total Comments: " + data.length ); ///num of list items
+        $('.comments').html("");
+        data.forEach(function(element,idx) {
+          var toDoStr = `<li data-id="${element._id}"> ${element.username}${element.comment}<a href=""> x</a></li>`
+          $('.comments').append(toDoStr)
+          doYouLikeMeme.comments.push(element);
+        });
+      },
+      error: function(err) {
+        console.error("ugh", err);
+      }
+    })
+  },
+  deletePost: function(commentId) {
+    var DeletePost = doYouLikeMeme.url + "/" + commentId;
+      $.ajax({
+        url: DeletePost,
+        method: "DELETE",
+        success: function(data) {
+          console.log("WE DELETED SOMETHING", data);
+          doYouLikeMeme.getPost();
+
         },
-        delete: function(toDoId) {
-            // find post to delete from our post data;
-            var DeletePost = gabeToDo.url + "/" + toDoId;
-            $.ajax({
-                url: DeletePost,
-                method: "DELETE",
-                success: function(data) {
-                    console.log("WE DELETED SOMETHING", data);
-                    gabeToDo.getToDo();
-
-                },
-                error: function(err) {
-                    console.error("ugh", err);
-                }
-            })
-        },
-
-        edit: function(thingToEdit) {
-            console.log("THING TO EDIT", thingToEdit);
-            $.ajax({
-                url: gabeToDo.url + "/" + thingToEdit._id,
-                method: "PUT",
-                data: thingToEdit,
-                success: function(data) {
-                    console.log("edited!!!!!", data)
-                    gabeToDo.getToDo();
-                },
-                error: function(err) {
-                    console.error("OH CRAP", err);
-                }
-            })
+        error: function(err) {
+          console.error("ugh", err);
         }
-      };
+      })
+    },
+
+    editPost: function(thingToEdit) {
+      console.log("THING TO EDIT", thingToEdit);
+      $.ajax({
+        url: doYouLikeMeme.url + "/" + thingToEdit._id,
+        method: "PUT",
+        data: thingToEdit,
+        success: function(data) {
+          console.log("edited!!!!!",data)
+          doYouLikeMeme.getPost();
+        },
+        error: function(err) {
+          console.error("OH CRAP", err);
+        }
+      })
+  },
+
+};
